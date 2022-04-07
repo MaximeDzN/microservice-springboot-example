49c33429161b13ecf89599ce6f3d63a956602bfd
maximeDzN <maxime.dazin@gmail.com>
2022-04-07 16:34:49 +0200

    MBEANS

--- src/main/java/fr/maximedzn/microservice/service/AccountServiceImpl.java
+++ src/main/java/fr/maximedzn/microservice/service/AccountServiceImpl.java
@@ -2,12 +2,14 @@ package fr.maximedzn.microservice.service;
 
 import fr.maximedzn.microservice.domain.Account.Account;
 import fr.maximedzn.microservice.repository.AccountRepository;
+import org.springframework.jmx.export.annotation.ManagedResource;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 
 @Service
 @Transactional
+@ManagedResource(objectName = "fr.maximedzn.microservice:name=AccountServiceImpl")
 public class AccountServiceImpl implements AccountService{
 
     private AccountRepository accountRepository;
